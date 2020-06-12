028744c6e4e691a78c642535407013b3213fb6cb
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-12 20:57:45 +0200

    test: Mocking

--- src/__tests__/ZeroToHero.spec.js
+++ src/__tests__/ZeroToHero.spec.js
@@ -20,3 +20,32 @@ for (let i = 0; i < pizzaTest.length; i += 1) {
     expect(pizzaTest[i]).toHaveProperty('price');
   });
 }
+
+test('mock implementation of a basic function', () => {
+  const mock = jest.fn(() => 'I am a mock');
+  expect(mock('Calling mock function')).toBe('I am a mock');
+  expect(mock).toHaveBeenCalledWith('Calling mock function');
+});
+
+test('mock return value of a function one time', () => {
+  const mock = jest.fn(() => 'I am a mock');
+
+  mock.mockReturnValueOnce('Hello').mockReturnValueOnce('My friend!');
+
+  mock();
+  mock();
+
+  expect(mock).toHaveBeenCalledTimes(2);
+
+  mock('Hello', 'My friend!', "What's Up?");
+  expect(mock).toHaveBeenCalledWith('Hello', 'My friend!', "What's Up?");
+
+  mock('Marvelous');
+  expect(mock).toHaveBeenLastCalledWith('Marvelous');
+});
+
+test('mock implementation of a basic function', () => {
+  const mock = jest.fn().mockImplementation(() => 'Spain');
+  expect(mock('Location')).toBe('Spain');
+  expect(mock).toHaveBeenCalledWith('Location');
+});
